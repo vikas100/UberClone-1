48da99079b1d61be0cde94477a3de5ff535666d8
Baskarant <baskar159@gmail.com>
2017-07-01 13:28:25 +0530

    Added SignUp & LogIn Code

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -11,53 +11,87 @@ import UIKit
 import Parse
 
 class ViewController: UIViewController {
+    
+    func displayAlert(title:String,message:String){
+        let alertController =  UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
 
+    var signUpMode = true
+    
     @IBOutlet weak var txtUserName: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
     @IBOutlet var isDriverSwitch: UISwitch!
     
     // button sign up action
     @IBAction func SignupOrLogin(_ sender: Any) {
-   
+        if txtUserName.text == "" || txtPassword.text == "" {
+            displayAlert(title: "Error in Form", message: "UserName and Password are Required")
+        } else{
+            if signUpMode {
+                let user = PFUser()
+                user.username = txtUserName.text
+                user.password = txtPassword.text
+                
+                user["isDriver"] = isDriverSwitch.isOn
+                
+                user.signUpInBackground(block: { (sucess,error)  in
+                    
+                    if let error = error {
+                        
+                        var  displayofErrorMessage = "Please try again later"
+                        
+                        if let parseError = (error as NSError).userInfo["error"] as? String {
+                            
+                            displayofErrorMessage = parseError
+                            
+                        }
+                        self.displayAlert(title: "SignUp Failed", message: displayofErrorMessage)
+                    } else {
+                        print("Sign Up SuccessFul")
+                    }
+                })
+            } else {
+                PFUser.logInWithUsername(inBackground: txtUserName.text!, password: txtPassword.text!, block: { (user,error) in
+                 
+                    if let error = error {
+                        
+                        var  displayofErrorMessage = "Please try again later"
+                        
+                        if let parseError = (error as NSError).userInfo["error"] as? String {
+                            displayofErrorMessage = parseError
+                        }
+                        self.displayAlert(title: "SignUp Failed", message: displayofErrorMessage)
+                    } else {
+                        print("Log In SuccessFul")
+                    }
+                    
+                })
+            }
+        }
     }
-    @IBOutlet var signupOrLogin: UIButton!
+    
+    @IBOutlet var signupOrLoginButton: UIButton!
     @IBOutlet var signupSwitchButton: UIButton!
     
     // button action switch to LogIn
     @IBAction func btnswitchSignUpMode(_ sender: Any) {
+        if signUpMode {
+            signupOrLoginButton.setTitle("Login", for: [])
+            signupSwitchButton.setTitle("Switch To Sign Up", for: [])
+            signUpMode =  false
+        } else {
+            signupOrLoginButton.setTitle("Sign Up", for: [])
+            signupSwitchButton.setTitle("Switch To Log In", for: [])
+            signUpMode =  true
+        }
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        let testObject = PFObject(className: "TestObject2")
-        
-        testObject["foo"] = "bar"
-        
-        testObject.saveInBackground { (success, error) -> Void in
-            
-            // added test for success 11th July 2016
-            
-            if success {
-                
-                print("Object has been saved.")
-                
-            } else {
-                
-                if error != nil {
-                    
-                    print (error)
-                    
-                } else {
-                    
-                    print ("Error")
-                }
-                
-            }
-            
-        }
         
     }
 
