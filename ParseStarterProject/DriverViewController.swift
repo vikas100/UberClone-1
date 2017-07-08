370e42e8a09a237836ec0f619defca32def25f29
Baskarant <baskar159@gmail.com>
2017-07-08 21:38:09 +0530

    Remove Unused Code in DriverViewController.swift

--- ParseStarterProject/DriverViewController.swift
+++ ParseStarterProject/DriverViewController.swift
@@ -53,13 +53,6 @@ class DriverViewController: UITableViewController, CLLocationManagerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
